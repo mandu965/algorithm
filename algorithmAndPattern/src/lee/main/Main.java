a26ac5bdb784a01cb398b82c61dde221644c938a
Lee Jung Hun <mandu965@naver.com>
2017-09-26 20:34:58 +0900

    fix gitignore

--- algorithmAndPattern/src/lee/main/Main.java
+++ algorithmAndPattern/src/lee/main/Main.java
@@ -2,9 +2,11 @@ package lee.main;
 
 public class Main {
 
+	// 브런치 테스트 http://gasaesososo.tistory.com/10
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi Lee");
+		
 
 	}
 

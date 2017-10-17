385da4ed33018fd139f3889ca8f7f897c1563870
Lee Jung Hun <mandu965@naver.com>
2017-10-17 19:49:24 +0900

    test 2

--- algorithmAndPattern/src/lee/main/Main.java
+++ algorithmAndPattern/src/lee/main/Main.java
@@ -7,7 +7,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("Hi Lee");
 		System.out.println("Hi eclipse new branch");
-
+		System.out.println("Hi eclipse new branch2");
 	}
 
 }
